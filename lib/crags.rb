b3d288dc95597e50d5eaf0553b02c27f5754326e
Justin Marney <justin.marney@viget.com>
2009-02-06 07:51:39 -0500

    geeemmmsppeeeek

--- lib/crags.rb
+++ lib/crags.rb
@@ -3,7 +3,7 @@ require 'curb'
 require 'hpricot'
 
 module Crags
-  VERSION = '1.2.2'
+  VERSION = '1.2.3'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
 
